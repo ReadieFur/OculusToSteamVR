bfe36f48e25bf6ed2dec8be270855956c4d07e4b
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-06-02 23:32:12 +0100

    Simplified vibration
    This comes at the cost of weaker vibrations but a faster response time. I would try to figure out how the old code managed to increase the intensity of the vibrations but it is hard for me to read/understand. I may come back to this at another point in the future.

--- src/OculusToSteamVRClient/OculusToSteamVRClient.cpp
+++ src/OculusToSteamVRClient/OculusToSteamVRClient.cpp
@@ -17,30 +17,6 @@
 
 #define RUN_INTERVAL 90 //Set to 90 becuase that is the max update rate of the Oculus sensors.
 
-uint32_t futureVibrationBufferIndex[2] = { 0 };
-double vibrationBufferTime[2] = { 0 };
-uint8_t futureVibrationBuffer[2][1024] = { {0}, {0} };
-std::vector<uint8_t> pulsePatterns[17] =
-{
-	{},
-	{ 255 },
-	{ 255, 0 },
-	{},
-	{ 196, 255, 128, 0 },
-	{},
-	{ 196, 255, 196, 128, 0, 0, 0},
-	{},
-	{ 196, 255, 255, 128, 64, 0, 0, 0},
-	{},
-	{ 196, 255, 255, 196, 128, 0, 0, 0, 0, 0 },
-	{},
-	{},
-	{},
-	{},
-	{},
-	{ 64, 128, 196, 255, 255, 128, 64, 32, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
-
 inline bool ShouldLog(int frameCount)
 {
 #if TRUE
@@ -48,99 +24,6 @@ inline bool ShouldLog(int frameCount)
 #else
 	return frameCount & 0x7FF;
 #endif
-}
-
-void AddVibration(bool isRightHand, float amplitude, float frequency, float duration)
-{
-	//std::cout << " adding haptic for amplitude " << amplitude << " frequency " << frequency << " duration " << duration << std::endl;
-	if (duration < 0) duration = 0;
-	float amp = amplitude / 100.0f;
-	float freq = frequency * 320.0f;
-	if (amplitude >= 100.0f) amp = 1.0f;
-	if (amp < 128.0 / 256) amp = 128.0 / 256;
-	uint32_t requestedDuration = duration * 320; // 320 Hz processing rate
-	uint32_t minDuration = 1;
-	uint32_t pulseWidth = 1;
-	if (freq < 160)
-	{
-		minDuration = 2;
-		pulseWidth = 1;
-	}
-	else if (freq < 80)
-	{
-		minDuration = 4;
-		pulseWidth = 2;
-	}
-	else if (freq < 60)
-	{
-		minDuration = 6;
-		pulseWidth = 3;
-	}
-	else if (freq < 40)
-	{
-		minDuration = 8;
-		pulseWidth = 4;
-	}
-	else if (freq < 32)
-	{
-		minDuration = 10;
-		pulseWidth = 5;
-	}
-	else if (freq < 20)
-	{
-		minDuration = 16;
-		pulseWidth = 8;
-	}
-
-	if (minDuration > requestedDuration) requestedDuration = minDuration;
-
-	//  std::cout << " adding haptic for amplitude " << amplitude << " -> " << amp << " frequency " << frequency << " -> " << freq << " duration " << duration << " ->" << requested_duration
-	  //    <<" pulse_width " << pulse_width << std::endl;
-
-	uint64_t vibrationBufferSampleDelta = ((ovr_GetTimeInSeconds() - vibrationBufferTime[isRightHand]) * 320);
-	//std::cout << " adding haptic for amplitude " << amplitude << " -> " << amp << " frequency " << frequency << " -> " << freq << " duration " << duration << " ->" << requestedDuration
-		//<< " pulse_width " << pulseWidth << " delta " << vib_buffer_sample_delta << std::endl;
-	for (int i = 0; i < requestedDuration; i++)
-	{
-		int index = (futureVibrationBufferIndex[isRightHand] + vibrationBufferSampleDelta + i);
-		futureVibrationBuffer[isRightHand][index % 1024] = pulsePatterns[minDuration][index % minDuration] * amp;
-	}
-}
-
-void VibrationThread(ovrSession mSession)
-{
-	unsigned char vibrationBuffer[8];
-	ovrHapticsBuffer oHapticsBuffer;
-	oHapticsBuffer.SamplesCount = 8;
-	oHapticsBuffer.Samples = vibrationBuffer;
-	oHapticsBuffer.SubmitMode = ovrHapticsBufferSubmit_Enqueue;
-	uint64_t counter = 0;
-	while (true)
-	{
-		for (int hand = 0; hand < 2; hand++)
-		{
-			for (int i = 0; i < 8; i++)
-			{
-				vibrationBuffer[i] = futureVibrationBuffer[hand][futureVibrationBufferIndex[hand] + i];
-				futureVibrationBuffer[hand][futureVibrationBufferIndex[hand] + i] = 0;
-			}
-			ovrTouchHapticsDesc desc = ovr_GetTouchHapticsDesc(mSession, hand == 0 ? ovrControllerType_LTouch : ovrControllerType_RTouch);
-			//std::cout << "SampleRate " << desc.SampleRateHz << " SampleSize " << desc.SampleSizeInBytes << " MaxSamples " << desc.SubmitMaxSamples << " MinSamples " << desc.SubmitMinSamples << " OptimalSamples " << desc.SubmitOptimalSamples << std::endl;
-
-			ovrHapticsPlaybackState oHapticsPlaybackState;
-			ovr_GetControllerVibrationState(mSession, hand == 0 ? ovrControllerType_LTouch : ovrControllerType_RTouch, &oHapticsPlaybackState);
-			//std::cout << (counter & 0xff) << " - queue state before  space: " << pbState.RemainingQueueSpace << "   samples: " << pbState.SamplesQueued << " / " << desc.QueueMinSizeToAvoidStarvation << std::endl;
-			ovr_SubmitControllerVibration(mSession, hand == 0 ? ovrControllerType_LTouch : ovrControllerType_RTouch, &oHapticsBuffer);
-			ovr_GetControllerVibrationState(mSession, hand == 0 ? ovrControllerType_LTouch : ovrControllerType_RTouch, &oHapticsPlaybackState);
-
-			//std::cout << (counter & 0xff) << " - queue state after   space: " << pbState.RemainingQueueSpace << "   samples: " << pbState.SamplesQueued << " / " << desc.QueueMinSizeToAvoidStarvation << std::endl;
-			futureVibrationBufferIndex[hand] += 8;
-			vibrationBufferTime[hand] = ovr_GetTimeInSeconds();
-			if (futureVibrationBufferIndex[hand] >= 1024) futureVibrationBufferIndex[hand] = 0;
-		}
-		counter++;
-		Sleep(25);
-	}
 }
 
 void VRLoop(ovrSession oSession, HANDLE sharedMutex, SharedData* sharedBuffer, uint64_t frameCount,
@@ -192,19 +75,15 @@ void VRLoop(ovrSession oSession, HANDLE sharedMutex, SharedData* sharedBuffer, u
 		//Update input state values.
 		sharedBuffer->oInputState[i] = oInputState;
 
-		//Set the vibration state.
-		//ovr_SetControllerVibration(oSession, i == 0 ? ovrControllerType::ovrControllerType_LTouch : ovrControllerType::ovrControllerType_RTouch,
-			//sharedBuffer->cHapticEventInfo[i].frequency, sharedBuffer->cHapticEventInfo[i].amplitude); //I read up that this is broken.
-
-		//No clue what this is doing.
+		//Vibration.
 		if (sharedBuffer->cHapticEventInfo[i].shouldVibrate)
 		{
-			AddVibration(i == 1, sharedBuffer->cHapticEventInfo[i].frequency, sharedBuffer->cHapticEventInfo[i].amplitude, sharedBuffer->cHapticEventInfo[i].duration);
 			sharedBuffer->cHapticEventInfo[i].shouldVibrate = false;
 
 			int duration = sharedBuffer->cHapticEventInfo[i].duration * 320;
 			if (duration > 128) duration = 128;
 			if (duration < 2) duration = 2;
+
 			oHapticsBuffer.SamplesCount = duration;
 			uint32_t ratio = 1;
 			if (sharedBuffer->cHapticEventInfo[i].frequency > 80/*300*/) ratio = 4;
@@ -213,6 +92,7 @@ void VRLoop(ovrSession oSession, HANDLE sharedMutex, SharedData* sharedBuffer, u
 			else ratio = 1;
 			for (int j = 0; j < duration; j++)
 			{
+				//No clue what this is doing.
 				if ((j & 3) < ratio)
 				{
 					float v = (0.1f + (sharedBuffer->cHapticEventInfo[i].amplitude * 0.9f)) * 255.0f;
@@ -225,7 +105,6 @@ void VRLoop(ovrSession oSession, HANDLE sharedMutex, SharedData* sharedBuffer, u
 				else hapticsBuffer[j] = 0;
 			}
 			ovr_SubmitControllerVibration(oSession, (i > 0) ? ovrControllerType_RTouch : ovrControllerType_LTouch, &oHapticsBuffer);
-			sharedBuffer->cHapticEventInfo[i].shouldVibrate = false;
 			for (int j = 0; j < hapticsBufferSize; j++) hapticsBuffer[j] = 255;
 		}
 
@@ -402,8 +281,6 @@ int main()
 		std::cout << "Failed to initialize OVR. Code " << oErrorInfo.Result << ". " << oErrorInfo.ErrorString << std::endl;
 		return 3;
 	}
-
-	std::thread vibrationThread(VibrationThread, oSession);
 
 	//Main loop.
 	uint64_t counter = 0;
